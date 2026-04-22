5219dc4257d745f505632ec2e83b5966448ebf11
takahashi kenzo <linkworks.sendai04@gmail.com>
2026-04-22 14:33:04 +0900

    update

--- functions.php
+++ functions.php
@@ -29,7 +29,7 @@ function mytheme_scripts()
   wp_enqueue_style('about',  get_theme_file_uri('/css/about.css'));
   wp_enqueue_style('works',  get_theme_file_uri('/css/works.css'));
   wp_enqueue_style('footer', get_theme_file_uri('/css/footer.css'));
-
+  wp_enqueue_style('header', get_theme_file_uri('/css/header.css'));
 
   wp_enqueue_script('works', get_theme_file_uri('/js/works.js'));
 }
--- header.php
+++ header.php
@@ -5,26 +5,31 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?php wp_title(); ?></title>
-  <mete name="description" content="<?php bloginfo('description'); ?>">
-    <link rel="icon" href="<?php echo esc_url(get_theme_file_uri('img/favicon.ico')); ?>">
-    <?php wp_head(); ?>
+  <meta name="description" content="<?php bloginfo('description'); ?>">
+  <link rel="icon" href="<?php echo esc_url(get_theme_file_uri('img/favicon.ico')); ?>">
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 
-  <header class="site-header">
-    <div class="header-inner">
+  <header class="site_header">
+    <div class="header_inner">
 
       <!-- ロゴ -->
-      <div class="site-logo">
-        <a href="<?php echo home_url(); ?>">
+      <div class="site_logo">
+        <a class="logo_link" href="<?php echo home_url(); ?>">
           <?php bloginfo('name'); ?>
         </a>
       </div>
 
       <!-- ナビゲーション -->
-      <nav class="site-nav">
-        <?php wp_nav_menu(['theme_location' => 'header-menu']); ?>
+      <nav class="site_nav">
+        <?php wp_nav_menu(
+          [
+            'theme_location' => 'header-menu', // どの登録メニューを使うか」
+            'menu_class' => 'nav_list'   // ulタグのクラス　ここまでしか出力できない。
+          ]
+        ); ?>
       </nav>
 
     </div>

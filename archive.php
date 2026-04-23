3292b397fc38bdc80ecfe6f5309c3ba275c41a17
takahashi kenzo <linkworks.sendai04@gmail.com>
2026-04-23 11:32:46 +0900

    update

---
+++ archive.php
@@ -0,0 +1,34 @@
+<?php get_header() ?>
+
+<main class="main">
+    <!-- ===============================================
+       News
+       =============================================== -->
+    <section class="news_area">
+        <h2 class="news_title">News</h2>
+        <dl class="news_list">
+            <div class="news_item">
+                <dt class="news_date"><time datetime="2020-10-10">2020.XX.XX</time></dt>
+                <dd class="news_description">デザイン雑誌「ＸＸＸＸＸＸＸ Vol.11」に掲載していただきました。</dd>
+            </div>
+            <div class="news_item">
+                <dt class="news_date"><time datetime="2020-09-10">2020.XX.XX</time></dt>
+                <dd class="news_description">ＸＸ月ＸＸ日から写真集「ＸＸＸＸＸＸＸ」の販売を開始します。</dd>
+            </div>
+            <div class="news_item">
+                <dt class="news_date"><time datetime="2019-10-10">2019.XX.XX</time></dt>
+                <dd class="news_description">【イベント開催のお知らせ】テキストテキストテキストテキストテキストテキストテキスト</dd>
+            </div>
+            <div class="news_item">
+                <dt class="news_date"><time datetime="2019-09-10">2019.XX.XX</time></dt>
+                <dd class="news_description">デザイン雑誌「ＸＸＸＸＸＸＸ Vol.10」に掲載していただきました。</dd>
+            </div>
+            <div class="news_item">
+                <dt class="news_date"><time datetime="2019-08-10">2019.XX.XX</time></dt>
+                <dd class="news_description">【個展開催のお知らせ】テキストテキストテキストテキストテキストテキストテキスト</dd>
+            </div>
+        </dl>
+    </section>
+</main>
+
+<?php get_footer() ?>
